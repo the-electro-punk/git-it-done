0243d7f01fdb9ed398a702f785dff376fcafafcc
the-electro-punk <bwr2555@hotmail.com>
2021-09-13 13:48:17 -0500

    code now tells if repository has issues

--- module-6-starter/assets/js/homepage.js
+++ module-6-starter/assets/js/homepage.js
@@ -48,6 +48,20 @@ var displayRepos = function(repos, searchTerm) {
     var titleEl = document.createElement("span");
     titleEl.textContent = repoName;
   
+    // create a status element
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
+
+    // check if current repo has issues or not
+    if (repos[i].open_issues_count > 0) {
+    statusEl.innerHTML =
+        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+    } else {
+    statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+    }
+
+// append to container
+repoEl.appendChild(statusEl);
     // append to container
     repoEl.appendChild(titleEl);
   
